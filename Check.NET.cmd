430ea0b75b3b78fc5a643b00a7e6f58737d86de6
Ivan Perehynets <jonilviv@gmail.com>
2017-10-28 19:07:54 +0200

    Added `.NET 4.7.1` checker

--- Check.NET.cmd
+++ Check.NET.cmd
@@ -13,5 +13,6 @@
 "NET4.6.1.exe" -c
 "NET4.6.2.exe" -c
 "NET4.7.exe" -c
+"NET4.7.1.exe" -c
 
 pause
---
+++ NET4.7.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(".NET 4.7.1")]
+[assembly: AssemblyProduct(".NET 4.7.1")]
+[assembly: AssemblyDescription(".NET 4.7.1 Checker")]
