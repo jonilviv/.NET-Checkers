810504924dc8d826a221b799599de54cde1dc079
Ivan Perehynets <jonilviv@gmail.com>
2017-10-28 18:05:10 +0200

    Added .NET 3.0 checker

--- Check.NET.cmd
+++ Check.NET.cmd
@@ -1,3 +1,5 @@
+@echo off
 "NET2.0.exe" -c
+"NET3.0.exe" -c
 
 pause
---
+++ NET3.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(".NET 3.0")]
+[assembly: AssemblyProduct(".NET 3.0")]
+[assembly: AssemblyDescription(".NET 3.0 Checker")]
