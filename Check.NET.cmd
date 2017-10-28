2a7d67b3e1286ebb3cef84e65a56760e8584bfb6
Ivan Perehynets <jonilviv@gmail.com>
2017-10-28 18:16:56 +0200

    Added `.NET 3.5 Client Profile` checker

--- Check.NET.cmd
+++ Check.NET.cmd
@@ -1,6 +1,8 @@
 @echo off
+
 "NET2.0.exe" -c
 "NET3.0.exe" -c
 "NET3.5.exe" -c
+"NET3.5 Client Profile.exe" -c
 
 pause
---
+++ NET3.5 Client Profile/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle(".NET 3.5 Client Profile")]
+[assembly: AssemblyProduct(".NET 3.5 Client Profile")]
+[assembly: AssemblyDescription(".NET 3.5 Client Profile Checker")]
