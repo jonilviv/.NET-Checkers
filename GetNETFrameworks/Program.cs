ba8ec300e9835749a9e6d9b151786e5a4ca49aff
Ivan Perehynets <jonilviv@gmail.com>
2017-12-20 10:11:04 +0100

    Added new project for getting .NET info from registry.

---
+++ GetNETFrameworks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace GetNETFrameworks
+{
+    public static class VersionTest
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Environment.Version = " + Environment.Version);
+
+            PauseCode();
+        }
+
+        [Conditional("DEBUG")]
+        private static void PauseCode()
+        {
+            Console.ReadKey();
+        }
+    }
+}
